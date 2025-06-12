f16c3289092206ce78d4493f0632471d3b22ba98
kuniquoc <102220122@sv1.dut.udn.vn>
2025-06-12 21:48:05 +0700

    Add exception handling infrastructure with middleware and custom exception classes

---
+++ quiz_application.Api/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+namespace quiz_application.Api.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseErrorHandling(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<ErrorHandlingMiddleware>();
+        }
+    }
+}
